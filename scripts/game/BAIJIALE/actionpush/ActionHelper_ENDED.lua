a764374159f40e07611dd799895e0003a4ee5473
antwand <antwand@sina.com>
2018-02-04 16:42:57 +0800

    action

--- scripts/game/BAIJIALE/actionpush/ActionHelper_ENDED.lua
+++ scripts/game/BAIJIALE/actionpush/ActionHelper_ENDED.lua
@@ -46,13 +46,18 @@ root.init = function(rid,round)
     
     
     
-    --计时器
+    --计时器  开始下一局 
     local func = function()  
         --清空旧局
         round:close();
-        --重新开始新局
-        local srv_game_action = skynet.call("srv_center", "lua", "getOneServer", "srv_game_action")
-        skynet.call(srv_game_action, "lua", "initByRound", rid)
+        
+        --开始创建新局 
+        local room = skynet.call(srv_hall_room, "lua", "getRoomByRoomId", rid)
+        if room then 
+            --重新开始新局
+            local srv_game_action = skynet.call("srv_center", "lua", "getOneServer", "srv_game_action")
+            skynet.call(srv_game_action, "lua", "initByRound", room)
+        end
     end
     local ti = config.next_round_timeout
     round:create_timeout(ti, func)
