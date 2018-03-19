e6cb7dbbd078f4c73ab616434b4e75a9bed58b96
antwand <antwand@sina.com>
2018-03-19 14:36:37 +0800

    change name

--- test/api/sprotodump-update/dump.sh
+++ test/api/sprotodump-update/dump.sh
@@ -1,1 +1,17 @@
-lua sprotodump.lua -spb ../../../proto/proto.c2s.sproto ../../../proto/proto.s2c.sproto -o sproto
+#!/bin/bash 
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+
+# proto 源文件 
+SOURCE_DIR="$DIR/../../../proto/"
+
+# 发布的 目录文件
+TARGET_DIR="$DIR/bin/sproto"
+
+
+cd "$DIR/run/" 
+
+
+
+
+./lua sprotodump.lua -spb $SOURCE_DIR/proto.c2s.sproto $SOURCE_DIR/proto.s2c.sproto -o $TARGET_DIR
